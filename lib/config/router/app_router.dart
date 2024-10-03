d53e93db62dfa8045d261484c34f67c641ceb5d5
Valeria R <v.rogatchevskikh@globant.com>
2024-10-03 21:37:11 +0200

    adds animated container

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -27,5 +27,9 @@ final appRouter = GoRouter(
         path: '/snackbars',
         name: SnackbarScreen.name,
         builder: (context, state) => const SnackbarScreen()),
+    GoRoute(
+        path: '/animated',
+        name: AnimatedScreen.name,
+        builder: (context, state) => const AnimatedScreen()),
   ],
 );
